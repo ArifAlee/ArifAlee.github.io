4abbc4d1678c6ba8ad0510fa5cf8f55b2fbe4c35
Arif Ali <Arifalee241@gmail.com>
2025-01-26 01:28:48 +0000

    classes, functions and comments
    Character class added, functions and comments

--- Star_Wars_Data_Cards/app.js
+++ Star_Wars_Data_Cards/app.js
@@ -1,11 +1,52 @@
+import { Characters } from './characters.js';
 
-const starwars = async function(){
-    try {
-        const res = await axios.get("https://swapi.dev/api/people/1/")
-        console.log(res.data)
-    } catch (error) {
-        console.log(error)
+const input = document.querySelector("#input");
+const submitBtn = document.querySelector("button");
+
+submitBtn.addEventListener("click", () => {
+  updateChar(input.value);
+});
+// Submit by pressing ENTER
+input.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    updateChar(input.value);
+  }
+});
+
+// Function updates h1 where name is displayed
+const updateChar = async function (name) {
+  const charName = await getName(name);
+  const charh1 = document.querySelector(".char-name");
+  charh1.textContent = charName;
+};
+
+
+// Get info from starwars api
+const getName = async function (num) {
+  const res = await axios.get(`https://swapi.dev/api/people/${num}`);
+  return res.data.name;
+};
+
+// Function retrieves all the names and stores it
+const allNames = async function () {
+  const charNames = [];
+  for (let i = 1; i < 84; i++) {
+    if(i === 17){ //no details found for #17, will be skipped
+      continue;
     }
-}
+    try {
+      const names = await getName(i);
+      charNames.push(names)
+    } catch (error) {
+      console.log("Error retrieving data", error);
+    }
+  }
+  return charNames;
+};
 
-starwars()
+allNames()
+
+const luke = new Characters("Luke Skywalker",172,77,"blond","fair","blue","19BBY","male","Tatooine")
+
+console.log(luke)
+
---
+++ Star_Wars_Data_Cards/characters.js
@@ -0,0 +1,13 @@
+export class Characters{
+    constructor(name,height,mass,hair,skin,eye,birth,gender,homeworld){
+      this.name = name;
+      this.height = height;
+      this.mass = mass;
+      this.hair = hair;
+      this.skin = skin;
+      this.eye = eye;
+      this.birth = birth;
+      this.gender = gender;
+      this.homeworld = homeworld;
+    }
+  }
