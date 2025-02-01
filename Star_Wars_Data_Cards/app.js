79d4f12985f0dc1920fb40aa6d37582b266ae6c2
Arif Ali <Arifalee241@gmail.com>
2025-02-01 00:26:28 +0000

    api data
    received and stored api data

--- Star_Wars_Data_Cards/app.js
+++ Star_Wars_Data_Cards/app.js
@@ -1,10 +1,11 @@
-import { Characters } from './characters.js';
+import { Characters } from "./characters.js";
 
 const input = document.querySelector("#input");
 const submitBtn = document.querySelector("button");
 
 submitBtn.addEventListener("click", () => {
-  updateChar(input.value);
+  const userInput = input.value;
+  updateChar(userInput);
 });
 // Submit by pressing ENTER
 input.addEventListener("keypress", (event) => {
@@ -15,38 +16,86 @@ input.addEventListener("keypress", (event) => {
 
 // Function updates h1 where name is displayed
 const updateChar = async function (name) {
-  const charName = await getName(name);
-  const charh1 = document.querySelector(".char-name");
-  charh1.textContent = charName;
+  try {
+    const charName = await getCharData(name);
+    const charh1 = document.querySelector(".char-name");
+    charh1.textContent = charName.name;
+  } catch (error) {
+    console.log("Error updating name", error);
+  }
 };
 
-
-// Get info from starwars api
-const getName = async function (num) {
-  const res = await axios.get(`https://swapi.dev/api/people/${num}`);
-  return res.data.name;
+// Get data from starwars api
+const getCharData = async function (num) {
+  try {
+    const res = await axios.get(`https://swapi.dev/api/people/${num}`);
+    return res.data;
+  } catch (error) {
+    console.log("Error fetching data", error);
+  }
 };
 
-// Function retrieves all the names and stores it
-const allNames = async function () {
-  const charNames = [];
+// Function retrieves all data and stores it
+const allData = async function () {
+  const charData = [];
   for (let i = 1; i < 84; i++) {
-    if(i === 17){ //no details found for #17, will be skipped
+    if (i === 17) {
+      //no details found for #17, will be skipped
       continue;
     }
     try {
-      const names = await getName(i);
-      charNames.push(names)
+      const data = await getCharData(i);
+      charData.push(data);
     } catch (error) {
       console.log("Error retrieving data", error);
     }
   }
-  return charNames;
+  return charData;
 };
 
-allNames()
+const getCharObj = async function (name) {
+  try {
+    const characters = await allData();
 
-const luke = new Characters("Luke Skywalker",172,77,"blond","fair","blue","19BBY","male","Tatooine")
+    return characters.find(
+      (character) => character.name.toLowerCase() === name.toLowerCase()
+    );
+    // console.log(characters.charData.map((char) => char.name))
+  } catch (error) {
+    console.log("Error creating object", error);
+  }
+};
 
-console.log(luke)
+async function character(input) {
+  const name = input;
+  const character = await getCharObj(name);
+  try {
+    console.log(character);
+  } catch (error) {
+    console.log("Character not found");
+  }
+}
+character("luke skywalker");
 
+const luke = new Characters(
+  "Luke Skywalker",
+  172,
+  77,
+  "blond",
+  "fair",
+  "blue",
+  "19BBY",
+  "male",
+  "Tatooine"
+);
+
+console.log(luke);
+
+// character.name,
+// character.height,
+// character.mass,
+// character.hair_color,
+// character.skin_color,
+// character.birth_year,
+// character.gender,
+// character.homeworld
